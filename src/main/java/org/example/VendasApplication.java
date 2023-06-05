9eaf09d7e5a4f2c048f975a66437bc0d866e9406
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2023-06-05 13:26:51 -0300

    Adicionado Cliente controller com metodos(Save/Delete) e Mapeamento Post

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -1,9 +1,7 @@
 package org.example;
 
 import org.example.domain.entity.Cliente;
-import org.example.domain.entity.Pedido;
 import org.example.domain.repository.Clientes;
-import org.example.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,45 +9,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos){
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
         return args -> {
-            System.out.println("Salvando Cliente");
-            clientes.save(new Cliente("Douglas"));
-            clientes.save(new Cliente("João"));
+            Cliente c = new Cliente(null,"Fulano");
+            clientes.save(c);
 
-            Cliente fulano = new Cliente("fulano");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-           /* Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-            System.out.println(cliente);
-            System.out.println(cliente.getPedidos());*/
-
-            pedidos.findByCliente(fulano).forEach(System.out::println);
-
-           /* List<Cliente> result = clientes.encontrarPorNome("Douglas");
-            result.forEach(System.out::println);*/
+            System.out.println(clientes.findById(1));
         };
     }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-
 
     }
 }
--- src/main/java/org/example/domain/entity/ItemPedido.java
+++ src/main/java/org/example/domain/entity/ItemPedido.java
@@ -19,4 +19,36 @@ public class ItemPedido {
 
     @Column
     private Integer quantitade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantitade() {
+        return quantitade;
+    }
+
+    public void setQuantitade(Integer quantitade) {
+        this.quantitade = quantitade;
+    }
 }
